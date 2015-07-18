13b02b1d4a0e87eedd3af4af85d5fc663c937cf4
EtayKampyle <etay.schur@kampyle.com>
2015-07-18 12:35:12 +0300

    Scafelding

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: etayschur
+ * Date: 7/18/15
+ * Time: 12:27 PM
+ */
+
+?>
+
+<!doctype html>
+<html ng-app="app" ng-controller="appCtrl">
+<head>
+    <script src="static/js/angular.min.js"></script>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="static/js/ui-bootstrap-tpls-0.13.0.min.js"></script>
+    <script src="static/js/app.js"></script>
+</head>
+<body>
+
+</body>
---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by etayschur on 7/18/15.
+ */
+
+
+angular.module('app', [])
+    .controller('appCtrl', ['$scope', '$timeout' , function($scope,$timeout) {
+
+
+
+
+    }]);
