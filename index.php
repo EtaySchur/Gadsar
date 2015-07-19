50553fbd27798cfb41bc5acf576f93ca7599f22c
EtayKampyle <etay.schur@kampyle.com>
2015-07-19 10:34:09 +0300

    Add More Files

--- index.php
+++ index.php
@@ -11,12 +11,231 @@
 <!doctype html>
 <html ng-app="app" ng-controller="appCtrl">
 <head>
+
+    <meta charset="utf-8" />
     <script src="static/js/angular.min.js"></script>
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="static/js/ui-bootstrap-tpls-0.13.0.min.js"></script>
+    <script src="bower_components/angular-translate/angular-translate.min.js"></script>
     <script src="static/js/app.js"></script>
+
+    <link type="text/css" rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css"/>
 </head>
 <body>
+    <div style="direction: rtl" class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+                <form name="userBasicDetails">
+                    <div class="form-group">
+                        <label for="userBasicDetails_id">{{'userBasicDetails_id' |  translate}}</label>
+                        <input type="text" class="form-control" id="userBasicDetails_id" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="userBasicDetails_bornCountry">{{'userBasicDetails_bornCountry' |  translate}}</label>
+                        <input type="text" class="form-control" id="userBasicDetails_bornCountry" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="userBasicDetails_fullName">{{'userBasicDetails_fullName' |  translate}}</label>
+                        <input type="text" class="form-control" id="userBasicDetails_fullName" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="userBasicDetails_birthYear">{{'userBasicDetails_birthYear' |  translate}}</label>
+                        <input type="text" class="form-control" id="userBasicDetails_birthYear" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="userBasicDetails_gender">{{'userBasicDetails_gender' |  translate}}</label>
+                        <input type="text" class="form-control" id="userBasicDetails_gender" placeholder="">
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-3">
+                <form name="advancedDetails">
+                    <div class="form-group">
+                        <label style="display: block" for="userAdvDetails_miluimQuestion">  {{'userAdvDetails_miluimQuestion' |  translate}}</label>
+                        <input type="radio" name="userAdvDetails_miluimQuestion_yes" ng-value="true" ng-model="formData.miluimQuestion">
+                        {{'general_yes' |  translate}}
+                        <input type="radio" name="userAdvDetails_miluimQuestion_no" ng-value="false" ng-model="formData.miluimQuestion">
+                        {{'general_no' |  translate}}
+                    </div>
+                    <div ng-if="formData.miluimQuestion" class="form-group">
+                        <label for="userAdvDetails_numberOfDays">{{'userAdvDetails_numberOfDays' |  translate}}</label>
+                        <input type="text" class="form-control" id="userAdvDetails_numberOfDays" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label style="display: block" for="userAdvDetails_abroadQuestion">{{'userAdvDetails_abroadQuestion' |  translate}}</label>
+                        <input type="radio" name="userAdvDetails_miluimQuestion_yes" ng-value="true" ng-model="formData.abroadQuestion">
+                        {{'general_yes' |  translate}}
+                        <input type="radio" name="userAdvDetails_miluimQuestion_no" ng-value="false" ng-model="formData.abroadQuestion">
+                        {{'general_no' |  translate}}
+                    </div>
+                    <div ng-if="formData.abroadQuestion" class="form-group">
+                        <label for="general_howManyTimes">{{'general_howManyTimes' |  translate}}</label>
+                        <input type="text" class="form-control" id="general_howManyTimes" placeholder="">
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-3">
+                <form name="studingDetails">
+                    <div class="form-group">
+                        <label for="studingDetails_city">{{'studingDetails_city' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_city" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="studingDetails_address">{{'studingDetails_address' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_address" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="studingDetails_zip">{{'studingDetails_zip' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_zip" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="general_phone">{{'general_phone' |  translate}}</label>
+                        <input type="text" class="form-control" id="general_phone" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label style="display: block" for="general_phone">{{'studingDetails_location_question' |  translate}}</label>
+                        <div>
+                            <input width="100%" type="radio" name="studingDetails_location_question" ng-value="true" ng-model="studingDetails.studingDetails_location_question">
+                            {{'studingDetails_location_answer_1' |  translate}}
+                        </div>
+                        <div>
+                            <input width="100%" type="radio" name="userAdvDetails_miluimQuestion_no" ng-value="false" ng-model="studingDetails.studingDetails_location_question">
+                            {{'studingDetails_location_answer_2' |  translate}}
+                        </div>
+                        <div>
+                            <input width="100%" type="radio" name="userAdvDetails_miluimQuestion_no" ng-value="false" ng-model="studingDetails.studingDetails_location_question">
+                            {{'studingDetails_location_answer_3' |  translate}}
+                        </div>
+                        <div>
+                            <input width="100%" type="radio" name="userAdvDetails_miluimQuestion_no" ng-value="false" ng-model="studingDetails.studingDetails_location_question">
+                            {{'studingDetails_location_answer_4' |  translate}}
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label style="display: block" for="studingDetails_carQuestion">{{'studingDetails_carQuestion' |  translate}}</label>
+                        <input type="radio" name="studingDetails_carQuestion_yes" ng-value="true" ng-model="formData.carQuestion">
+                        {{'general_yes' |  translate}}
+                        <input type="radio" name="studingDetails_carQuestion_no" ng-value="false" ng-model="formData.carQuestion">
+                        {{'general_no' |  translate}}
+                        <p  ng-if="formData.carQuestion"> {{'studingDetails_carQuestion_alert' |  translate}} </p>
+                    </div>
+                    <div ng-if="formData.carQuestion" class="form-group">
+                        <label for="studingDetails_carQuestion_model">{{'studingDetails_carQuestion_model' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_carQuestion_model" placeholder="">
+                    </div>
+                    <div ng-if="formData.carQuestion" class="form-group">
+                        <label for="studingDetails_carQuestion_value">{{'studingDetails_carQuestion_value' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_carQuestion_value" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label style="display: block" for="studingDetails_carQuestion_extraCar">{{'studingDetails_carQuestion_extraCar' |  translate}}</label>
+                        <input  type="radio" name="carQuestion_extraCar_yes" ng-value="true" ng-model="formData.carQuestion_extraCar">
+                        {{'general_yes' |  translate}}
+                        <input type="radio" name="carQuestion_extraCar_no" ng-value="false" ng-model="formData.carQuestion_extraCar">
+                        {{'general_no' |  translate}}
+                    </div>
+                    <div ng-if="formData.carQuestion_extraCar" class="form-group">
+                        <label for="studingDetails_carQuestion_extraCar">{{'studingDetails_carQuestion_model' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_carQuestion_extraCar" placeholder="">
+                    </div>
+                    <div ng-if="formData.carQuestion_extraCar" class="form-group">
+                        <label for="studingDetails_carQuestion_extraCarYear">{{'studingDetails_carQuestion_extraCarYear' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_carQuestion_extraCarYear" placeholder="">
+                    </div>
+                    <div ng-if="formData.carQuestion_extraCar" class="form-group">
+                        <label for="studingDetails_carQuestion_alert_extraCarNumber">{{'studingDetails_carQuestion_alert_extraCarNumber' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_carQuestion_alert_extraCarNumber" placeholder="">
+                    </div>
+                    <div ng-if="formData.carQuestion_extraCar" class="form-group">
+                        <label for="studingDetails_carQuestion_extraCar">{{'studingDetails_carQuestion_value' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_carQuestion_extraCar" placeholder="">
+                    </div>
+                    <p> {{'studingDetails_carQuestion_comment1' |  translate}} </p>
+                    <p> {{'studingDetails_carQuestion_comment2' |  translate}} </p>
+                </form>
+            </div>
+            <div class="col-md-3">
+                <form name="jobDetails">
+                    <div  class="form-group">
+                        <label style="display: block" for="jobDetails_hasJobQuestion">{{'jobDetails_hasJobQuestion' |  translate}}</label>
+                        <input  type="radio" name="carQuestion_extraCar_yes" ng-value="false" ng-model="formData.hasJobQuestion">
+                        {{'general_no' |  translate}}
+                        <input type="radio" name="carQuestion_extraCar_general" ng-value="'general'" ng-model="formData.hasJobQuestion">
+                        {{'general_yes' |  translate}}
+                        <input type="radio" name="carQuestion_extraCar_sideJob" ng-value="'sideJob'" ng-model="formData.hasJobQuestion">
+                        {{'jobDetails_hasJobAnswer1' |  translate}}
+                    </div>
+                    <div ng-if="formData.hasJobQuestion == 'general'" class="form-group">
+                        <label for="jobDetails_jobLocation">{{'jobDetails_jobLocation' |  translate}}</label>
+                        <input type="text" class="form-control" id="jobDetails_jobLocation" placeholder="">
+                    </div>
+                    <div ng-if="formData.hasJobQuestion == 'general'" class="form-group">
+                        <label for="studingDetails_address">{{'studingDetails_address' |  translate}}</label>
+                        <input type="text" class="form-control" id="studingDetails_address" placeholder="">
+                    </div>
+                    <div ng-if="formData.hasJobQuestion == 'general'" class="form-group">
+                        <label for="general_phone">{{'general_phone' |  translate}}</label>
+                        <input type="text" class="form-control" id="general_phone" placeholder="">
+                    </div>
+                    <div ng-if="formData.hasJobQuestion == 'general'" class="form-group">
+                        <label for="jobDetails_salary">{{'jobDetails_salary' |  translate}}</label>
+                        <input type="text" class="form-control" id="jobDetails_salary" placeholder="">
+                        <p> {{'jobDetails_salary_comment' |  translate}} </p>
+                    </div>
+                    <div ng-if="formData.hasJobQuestion == 'sideJob'" class="form-group">
+                        <label for="jobDetails_jobLocation">{{'jobDetails_jobLocation' |  translate}}</label>
+                        <input type="text" class="form-control" id="jobDetails_jobLocation" placeholder="">
+                    </div>
+                    <div ng-if="formData.hasJobQuestion == 'sideJob'" class="form-group">
+                        <label for="jobDetails_salary_amount">{{'jobDetails_salary_amount' |  translate}}</label>
+                        <input type="text" class="form-control" id="jobDetails_salary_amount" placeholder="">
+                        <p> {{'jobDetails_salary_comment2' |  translate}} </p>
+                    </div>
+                    <div  class="form-group">
+                        <label style="display: block" for="jobDetails_salary_depths">{{'jobDetails_salary_depths' |  translate}}</label>
+                        <p> {{'jobDetails_salary_depths_comment' |  translate}} </p>
+                        <input  type="radio" name="jobDetails_salary_depths_yes" ng-value="true" ng-model="formData.jobDetails_salary_depths">
+                        {{'general_yes' |  translate}}
+                        <input type="radio" name="jobDetails_salary_depths_no" ng-value="false" ng-model="formData.jobDetails_salary_depths">
+                        {{'general_no' |  translate}}
+                    </div>
+                    <div ng-if="formData.jobDetails_salary_depths" class="form-group">
+                        <label for="jobDetails_salary_depths_amount">{{'jobDetails_salary_depths_amount' |  translate}}</label>
+                        <input type="text" class="form-control" id="jobDetails_salary_depths_amount" placeholder="">
+                    </div>
+                    <div ng-if="formData.jobDetails_salary_depths" class="form-group">
+                        <label for="jobDetails_salary_depths_purpose">{{'jobDetails_salary_depths_purpose' |  translate}}</label>
+                        <input type="text" class="form-control" id="jobDetails_salary_depths_purpose" placeholder="">
+                    </div>
+                    <div ng-if="formData.jobDetails_salary_depths" class="form-group">
+                        <label for="jobDetails_salary_depths_time">{{'jobDetails_salary_depths_time' |  translate}}</label>
+                        <input type="text" class="form-control" id="jobDetails_salary_depths_time" placeholder="">
+                        <p> {{'jobDetails_salary_depths_comment2' | translate}}</p>
+                    </div>
+                    <div  class="form-group">
+                        <label style="display: block" for="jobDetails_salary_fundingQuestion">{{'jobDetails_salary_fundingQuestion' |  translate}}</label>
+                        <input  type="radio" name="jobDetails_salary_fundingQuestion_yes" ng-value="true" ng-model="formData.jobDetails_salary_fundingQuestion">
+                        {{'general_yes' |  translate}}
+                        <input type="radio" name="jobDetails_salary_fundingQuestion_no" ng-value="false" ng-model="formData.jobDetails_salary_fundingQuestion">
+                        {{'general_no' |  translate}}
+                    </div>
+                    <div ng-if="formData.jobDetails_salary_fundingQuestion" class="form-group">
+                        <label for="jobDetails_salary_depths_amount">{{'jobDetails_salary_depths_amount' |  translate}}</label>
+                        <input type="text" class="form-control" id="jobDetails_salary_depths_amount" placeholder="">
+                    </div>
+                    <div ng-if="formData.jobDetails_salary_fundingQuestion" class="form-group">
+                        <label style="display: block" for="jobDetails_salary_fundingSourceQuestion">{{'jobDetails_salary_fundingSourceQuestion' |  translate}}</label>
+                        <input  type="radio" name="jobDetails_salary_fundingQuestion_yes" value="{{'jobDetails_salary_fundingSourceAnswer1' |  translate}}" ng-model="formData.jobDetails_salary_fundingSourceQuestion">
+                        {{'jobDetails_salary_fundingSourceAnswer1' |  translate}}
+                        <input type="radio" name="jobDetails_salary_fundingQuestion_no" value="{{'jobDetails_salary_fundingSourceAnswer2' |  translate}}" ng-model="formData.jobDetails_salary_fundingSourceQuestion">
+                        {{'jobDetails_salary_fundingSourceAnswer2' |  translate}}
+                    </div>
+                </form>
+            </div>
 
+
+
+        </div>
+    </div>
 </body>
--- static/js/app.js
+++ static/js/app.js
@@ -3,10 +3,85 @@
  */
 
 
-angular.module('app', [])
-    .controller('appCtrl', ['$scope', '$timeout' , function($scope,$timeout) {
+angular.module('app', ['pascalprecht.translate'])
+    .config(function($translateProvider) {
+        $translateProvider.translations('heb', {
+            userBasicDetails_id: 'ת.ז. - כולל ספרת ביקורת',
+            userBasicDetails_bornCountry: ' ארץ לידה',
+            userBasicDetails_fullName: 'שם + שם משפחה',
+            userBasicDetails_birthYear: 'שנת לידה',
+            userBasicDetails_gender: 'מין',
+            userAdvDetails_miluimQuestion: 'האם במהלך השנה האחרונה שירתת במילואים?',
+            userAdvDetails_abroadQuestion: 'האם במהלך השנה האחרונה שהית בחו"ל?',
+            userAdvDetails_numberOfDays: "מס' ימים ?",
+            userAdvDetails_miluimQuestion: 'האם במהלך השנה האחרונה שירתת במילואים?',
+            general_yes: 'כן',
+            general_no: 'לא',
+            general_phone: 'טלפון',
+            general_howManyTimes: 'כמה פעמים ?',
+            general_whatPurpose: 'לאיזו מטרה ?',
+            studingDetails_city: 'היישוב',
+            studingDetails_address: 'כתובת',
+            studingDetails_zip: 'מיקוד',
+            studingDetails_location_question: 'בשנת הלימודים הנוכחית הנך גר ב',
+            studingDetails_location_answer_1: 'שכר דירה - יש לצרף חוזה',
+            studingDetails_location_answer_2: 'מעונות - יש לצרף חוזה',
+            studingDetails_location_answer_3: 'דירה בבעלותך',
+            studingDetails_location_answer_4: 'בית הורים או קרובי משפחה אחרים',
+            studingDetails_carQuestion: 'האם בבעלותך או ברשותך* או בבעלות או ברשות בן/ת זוגך** רכב?',
+            studingDetails_carQuestion_alert: 'יש לצרף צילום של רישיון הרכב',
+            studingDetails_carQuestion_model: 'סוג הרכב',
+            studingDetails_carQuestion_value: 'ערך הרכב',
+            studingDetails_carQuestion_extraCar: 'האם קיים במשפחה רכב נוסף - מתייחס לבן\\בת הזוג של הסטודנט\\ית',
+            studingDetails_carQuestion_extraCarYear: 'שנת ייצור',
+            studingDetails_carQuestion_alert_extraCarNumber: "מס' רישוי",
+            studingDetails_carQuestion_comment1: '* ברשות בן/בת זוג לרבות אדם החי עמך חיי משפחה במשק בית משותף.',
+            studingDetails_carQuestion_comment2: '** לרבות רכב השייך למוסד או לאדם אחר או גוף אחר, העומד לרשותך דרך קבע.',
+            jobDetails_hasJobQuestion: 'האם בשנת הלימודים הנוכחית הנך עובד/ת?',
+            jobDetails_hasJobAnswer1: 'כן , עבודה מזדמנת',
+            jobDetails_hasJobAnswer2: 'האם בשנת הלימודים הנוכחית הנך עובד/ת?',
+            jobDetails_jobLocation: 'מקום העבודה',
+            jobDetails_salary: 'מהו השכר החודשי',
+            jobDetails_salary_comment: 'יש לצרף שלושה תלושים אחרונים או אישור ממס ההכנסה',
+            jobDetails_salary_comment2: 'יש לצרף תצהיר על גובה ההכנסה',
+            jobDetails_salary_amount: 'הערכת שכר',
+            jobDetails_salary_depths: 'האם הנך משלם החזר חובות, לא כולל משכנתא ?',
+            jobDetails_salary_depths_comment: 'יש לצרף אישור מהבנק',
+            jobDetails_salary_depths_amount: 'על סך',
+            jobDetails_salary_depths_purpose: 'לאיזו  מטרה ?',
+            jobDetails_salary_depths_time: 'הזמן שנותר לסיום',
+            jobDetails_salary_depths_comment2: 'אם הנך משתתף בתשלום המשכנתא של ההורים עליך לצרף אישור מהבנק.',
+            jobDetails_salary_fundingQuestion: 'האם הנך מקבל מימון כלשהו ?',
+            jobDetails_salary_fundingSourceQuestion: 'ציין את מקור המימון',
+            jobDetails_salary_fundingSourceAnswer1: 'פנסיה',
+            jobDetails_salary_fundingSourceAnswer2: 'ביטוח לאומי',
+            jobDetails_salary_fundingSourceAnswer3: 'מזונות',
+            jobDetails_salary_fundingSourceAnswer4: 'משרד הבטחון/משרד הקליטה',
+            jobDetails_salary_fundingSourceAnswer5: 'תרומה',
+            jobDetails_salary_fundingSourceAnswer6: 'מקום עבודה',
+            jobDetails_salary_fundingSourceAnswer7: 'אחר',
+            jobDetails_salary_fundingSourceAnswerComment: 'יש לצרף אישורים',
+            jobDetails_salary_amount2: 'האם הנך משלם החזר חובות, לא כולל משכנתא',
+            jobDetails_salary_amount2: 'האם הנך משלם החזר חובות, לא כולל משכנתא',
+            jobDetails_salary_amount2: 'האם הנך משלם החזר חובות, לא כולל משכנתא',
+
+            INTRO_TEXT: 'And it has i18n support!'
+        });
+        $translateProvider.preferredLanguage('heb');
+})
+    .controller('appCtrl', ['$scope', '$timeout' , '$http', function($scope,$timeout,$http) {
+
+    $scope.formData = {};
+    $scope.var = "פואד";
 
 
-
+    $scope.lang = {
+        "userBasicDetails":{
+            "id":"אבי נימני",
+            "bornCountry":"",
+            "fullName":"",
+            "birthYear":""
+        }
+    }
 
     }]);
