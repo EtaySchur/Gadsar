bb53e25ca323015e074d8b705fdcd6ff1d9016ab
EtayKampyle <etay.schur@kampyle.com>
2015-07-24 15:52:23 +0300

    change server

--- server/db/manager.php
+++ server/db/manager.php
@@ -11,17 +11,17 @@ class DbManager {
 	private static function connectToDb() {
 
     /* EC2 */
-        /*
+
         $dbpass = 'Avishaynimni88';
         $dbhost = '127.0.0.1';
         $dbname = 'GadsarGolani';
         $dbuser = 'etay';
-        */
+        
          //LOCAL
-		$dbpass = 'Kulanu$namut09';
-		$dbhost = 'localhost';
-		$dbname = 'Gadsar';
-		$dbuser = 'root';
+		//$dbpass = 'Kulanu$namut09';
+		//$dbhost = 'localhost';
+		////$dbname = 'Gadsar';
+		//$dbuser = 'root';
 		$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass ,  array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		return $pdo;
 	}
